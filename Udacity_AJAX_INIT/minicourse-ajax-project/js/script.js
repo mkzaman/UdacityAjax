b080572a927aea13669ae4c3da52f14c82d3d0df
Zaman <mkzamanalive@gmail.com>
2017-12-19 17:19:01 +0600

    access google map api

--- Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
+++ Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
@@ -13,6 +13,15 @@ function loadData() {
 
     // load streetview
 
+    $('.bgImg').remove();
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var imagesource = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + street + ',' + city;
+
+    $body.append('<img class="bgImg" height=100% width=100% src="'+imagesource+'">');
+
+    $greeting.text('So you want to live in ' + street + ' ' + city + '?');
+
     // YOUR CODE GOES HERE!
 
     return false;
